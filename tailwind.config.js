43bdb6a07fae1ba476b30a9954ef8a9c9faa5218
Kretsul-Vitaliy <turistvital1988@gmail.com>
2023-03-29 08:54:51 +0300

    practic-2

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,23 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html'],
+  // darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      gridTemplateRows: {
+        layout: 'auto 1fr auto',
+      },
+      spacing: {
+        DEFAULT: '1rem',
+        7: '1.75rem',
+      },
+      colors: {
+        'logo-main': '#9b0404',
+      },
+      fontFamily: {
+        main: ['Montserrat'],
+      },
+    },
   },
   plugins: [],
 };
